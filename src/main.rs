24ddfaefedb203cffd2ddc65f96dc0041f1780ba
seres <seres@seres.eu.org>
2025-11-19 19:36:35 +0100

    added reply to analyze feature

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,72 @@ use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
+async fn parse_logfile(ctx: &Context, msg: &Message) {
+    let file = msg.attachments.get(0).unwrap();
+    if file.filename.ends_with(".jpg")
+        || file.filename.ends_with(".jpeg")
+        || file.filename.ends_with(".png")
+    {
+        return;
+    }
+    let file_content = match file.download().await {
+        Ok(c) => c,
+        Err(e) => {
+            println!("{e}");
+            let _ = msg.reply(&ctx.http, "Failed to download file.");
+            return;
+        }
+    };
+    let file_string = match str::from_utf8(&file_content) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{e}");
+            let _ = msg.reply(&ctx.http, "Failed to read file.");
+            return;
+        }
+    };
+
+    let lines: Vec<&str> = file_string.lines().collect();
+
+    let mut msg_content = String::new();
+
+    let mut stacktrace_pos = 0;
+    let mut found_exception_line = false;
+    for (i, line) in lines.iter().enumerate() {
+        if line.contains("intercepted unhandled hardware exception") {
+            let pos = line.find("\"").unwrap();
+            let (_, to_print) = line.split_at(pos);
+            msg_content.push_str(to_print);
+            msg_content.push('\n');
+            found_exception_line = true;
+        }
+        if line.contains("RVA") && found_exception_line {
+            stacktrace_pos = i + 2;
+            break;
+        }
+    }
+    let mut culprits: HashSet<&str> = HashSet::new();
+    for line in &lines[stacktrace_pos..] {
+        let split = line.split_whitespace().collect::<Vec<&str>>();
+        if split.len() == 4 {
+            let culprit = split.get(3).unwrap();
+            let culprit_lowercase = culprit.to_lowercase();
+            if culprit_lowercase == "ntdll" || culprit_lowercase == "kernel32" {
+                continue;
+            }
+            culprits.insert(culprit);
+        }
+    }
+    msg_content.push_str("\nLikely culprits:\n");
+    for culprit in culprits {
+        msg_content.push_str(culprit);
+        msg_content.push('\n');
+    }
+    if let Err(why) = msg.reply(&ctx.http, msg_content).await {
+        println!("Error sending message: {why:?}");
+    }
+}
+
 struct Handler;
 
 #[async_trait]
@@ -17,68 +83,11 @@ impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!analyze" {
             if msg.attachments.len() == 1 {
-                let file = msg.attachments.get(0).unwrap();
-                if file.filename.ends_with(".jpg")
-                    || file.filename.ends_with(".jpeg")
-                    || file.filename.ends_with(".png")
-                {
-                    return;
-                }
-                let file_content = match file.download().await {
-                    Ok(c) => c,
-                    Err(e) => {
-                        println!("{e}");
-                        let _ = msg.reply(&ctx.http, "Failed to download file.");
-                        return;
-                    }
-                };
-                let file_string = match str::from_utf8(&file_content) {
-                    Ok(v) => v,
-                    Err(e) => {
-                        println!("{e}");
-                        let _ = msg.reply(&ctx.http, "Failed to read file.");
-                        return;
-                    }
-                };
-
-                let lines: Vec<&str> = file_string.lines().collect();
-
-                let mut msg_content = String::new();
-
-                let mut stacktrace_pos = 0;
-                let mut found_exception_line = false;
-                for (i, line) in lines.iter().enumerate() {
-                    if line.contains("intercepted unhandled hardware exception") {
-                        let pos = line.find("\"").unwrap();
-                        let (_, to_print) = line.split_at(pos);
-                        msg_content.push_str(to_print);
-                        msg_content.push('\n');
-                        found_exception_line = true;
-                    }
-                    if line.contains("RVA") && found_exception_line {
-                        stacktrace_pos = i + 2;
-                        break;
-                    }
-                }
-                let mut culprits: HashSet<&str> = HashSet::new();
-                for line in &lines[stacktrace_pos..] {
-                    let split = line.split_whitespace().collect::<Vec<&str>>();
-                    if split.len() == 4 {
-                        let culprit = split.get(3).unwrap();
-                        let culprit_lowercase = culprit.to_lowercase();
-                        if culprit_lowercase == "ntdll" || culprit_lowercase == "kernel32" {
-                            continue;
-                        }
-                        culprits.insert(culprit);
-                    }
-                }
-                msg_content.push_str("\nLikely culprits:\n");
-                for culprit in culprits {
-                    msg_content.push_str(culprit);
-                    msg_content.push('\n');
-                }
-                if let Err(why) = msg.reply(&ctx.http, msg_content).await {
-                    println!("Error sending message: {why:?}");
+                parse_logfile(&ctx, &msg).await;
+            } else if msg.referenced_message.is_some() {
+                let referenced_msg = msg.referenced_message.unwrap();
+                if referenced_msg.attachments.len() == 1 {
+                    parse_logfile(&ctx, &referenced_msg).await;
                 }
             } else {
                 if let Err(why) = msg
